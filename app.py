b8b33fc3abe22f07ea24412aaf4336f6252f0160
Robert Machamud <robert_machamud@yahoo.com>
2022-04-27 19:51:11 +0000

    heroku deploy vivien schulz

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def produkt(produkt_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
